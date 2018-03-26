198de5542214c4109509f7e187e03a3909baba37
Jonathan Luk <jluk87@gmail.com>
2018-03-26 20:34:05 +0000

    automatically backed up by learn

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,17 @@
+module findable
+
+  ######### INSTANCE METHODS #########
+  
+  module InstanceMethods
+
+
+  end
+
+  ########## CLASS METHODS ##########
+
+  module ClassMethods
+
+
+  end
+
+end
